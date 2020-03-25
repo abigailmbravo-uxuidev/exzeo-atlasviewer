3ce07938dfc34c4ccaaa23c1ab218de03bea1f15
jsutphin <>
2020-03-25 07:57:17 -0400

    tab updates

---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 565.01 100">
+      <defs><style>.cls-1{fill:#f28f20;}.cls-2{fill:#84be41;}.cls-3,.cls-5{fill:#223241;}.cls-4{fill:#d85757;}.cls-5{font-size:85px;font-family:Lato-Black, Lato;font-weight:800;}.cls-6{font-family:Lato-Italic, Lato;font-weight:400;font-style:italic;}</style></defs>
+      <path class="cls-1" d="M49.39,35.22,74.72,9.89a50.06,50.06,0,0,0-45.84,4.82Z" transform="translate(-6.46 -6.44)"/><path class="cls-2" d="M35.24,49.36l-20.5-20.5A50.06,50.06,0,0,0,9.91,74.7Z" transform="translate(-6.46 -6.44)"/><path class="cls-3" d="M98.18,84a50,50,0,0,0-6.36-62.93L63.53,49.36Z" transform="translate(-6.46 -6.44)"/><path class="cls-4" d="M49.39,63.51,21.1,91.79A50,50,0,0,0,84,98.16Z" transform="translate(-6.46 -6.44)"/><text class="cls-5" transform="translate(147.16 77.66)">atlas<tspan class="cls-6" x="187.25" y="0">viewer</tspan></text></svg>
+  );
+};
+
+export default Logo;
