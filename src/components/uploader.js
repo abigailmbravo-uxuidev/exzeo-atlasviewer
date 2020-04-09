06786747ba6fad77b069a50c670ca80b02fdc66e
elamison <elamison@exzeo>
2020-04-09 09:49:24 -0400

    Hook up upload form.

--- src/components/uploader.js
+++ src/components/uploader.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import { useForm } from 'react-hook-form';
 import Papa from 'papaparse';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import { useLayerDispatch } from '../context/layer-context';
 
 const Uploader = () => {
+  const { register, handleSubmit, errors, formState } = useForm();
   const [file, setFile] = useState({});
   const [headers, setHeaders] = useState(null);
   const [layerData, setLayerData] = useState();
@@ -58,9 +59,9 @@ const Uploader = () => {
     return dispatch({ type: 'add', layer });
   };
 
-  const handleUpload = e => {
-    e.preventDefault();
-    console.log('data: ', e.target.feed.value);
+  const handleUpload = async data => {
+    const url = `${process.env.API_URL}/api/upload`;
+    const res = await axios.post(url, { data });
   };
 
   const processFile = e => {
@@ -78,21 +79,26 @@ const Uploader = () => {
 
   return (
     <div className="modal">
-      <form onSubmit={handleUpload}>
+      <form onSubmit={handleSubmit(handleUpload)}>
         <input
           id="feed"
           name="feed"
           type="file"
           accept="text/csv"
+          ref={register}
           onChange={handleFile}
         />
         <input
           type="text"
           id="feed-name"
           name="feed-name"
+          ref={register({ required: true })}
           defaultValue={file.name}
         />
-        <button>Upload</button>
+        {errors.lastname && 'Feed Name is required.'}
+        <button type="submit" enabled={formState.dirty}>
+          Upload
+        </button>
       </form>
     </div>
   );
