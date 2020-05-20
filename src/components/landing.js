9a18ec926fef9a5bb6defb34d2b3691a9e9de7bd
jsutphin-1 <jsutphin@exzeo.com>
2020-05-20 08:58:23 -0400

    update landing and buttons

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './logo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAtlas } from '@fortawesome/free-solid-svg-icons';
 import { useAuth } from '../context/auth-context';
@@ -8,12 +9,12 @@ const Landing = () => {
   const { loginWithRedirect } = useAuth();
   return (
     <div className="landingPage">
-      <div>
-        <FontAwesomeIcon icon={faAtlas} />
+      <div className="card">
+        <Logo />
         <h3>Welcome to Atlas!</h3>
-        <p>
-          <button type="button" onClick={() => loginWithRedirect()}>Login</button>
-        </p>
+        <button type="button" onClick={() => loginWithRedirect()}>
+          Login
+        </button>
       </div>
     </div>
   );
