fb1861b3d2263db24676e898aeebb650a18a76cc
jsutphin-1 <jsutphin@exzeo.com>
2020-05-26 15:41:42 -0400

    WIP data pop-out

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSignOutAlt,
+  faSlashEye,
+  faEye,
+  faTimes
+} from '@fortawesome/free-solid-svg-icons';
 import Map from './map';
 import Library from './library';
 import View from './view';
@@ -24,6 +29,48 @@ const Canvas = () => {
       </button>
       <div id="map-canvas">
         <Map basemap={basemap} />
+      </div>
+      <div className="feed-popOut">
+        <header>
+          <span className="gripper">:::::::::::::::::::</span>
+          <button className="iconBtn closeBtn" type="button">
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </header>
+        <div className="content">
+          <span className="eyeball-wrapper wrapper">
+            <FontAwesomeIcon icon={faEye} />
+            {/* toggle eye icon={faSlashEye} */}
+          </span>
+
+          <table>
+            <tr>
+              <th colSpan="2">
+                <h5>[ Feed Name ]</h5>
+              </th>
+              <th>column 2</th>
+              <th>column 3</th>
+            </tr>
+            <tr>
+              <td className="toggle">
+                <FontAwesomeIcon icon={faEye} />
+                {/* toggle eye icon={faSlashEye} */}
+              </td>
+              <td>column 1</td>
+              <td>column 2</td>
+              <td>column 3</td>
+            </tr>
+            <tr>
+              <td className="toggle">
+                <FontAwesomeIcon icon={faEye} />
+                {/* toggle eye icon={faSlashEye} */}
+              </td>
+              <td>column 1</td>
+              <td>column 2</td>
+              <td>column 3</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </LayerProvider>
   );
