9de865b663888111d24a54a136542bc81a55e31b
jsutphin-1 <jsutphin@exzeo.com>
2020-06-19 13:41:34 -0400

    strip br rom data

--- src/components/marker-popup.js
+++ src/components/marker-popup.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function strip(html) {
+  var doc = new DOMParser().parseFromString(html, 'text/html');
+  return doc.body.textContent || '';
+}
+
 const MarkerPopup = ({ properties }) => {
   return (
     <div className="marker-popup">
-      <h2>{properties.Name.replace(/<[^>]*>?/gm, ' ')}</h2>
+      <h2>{properties.Name.replace('<br/>', ' ')}</h2>
       <div className="popupContent">
         <ul>
           {properties &&
             Object.entries(properties).map(([key, value]) => (
               <li key={key}>
-                <span>{key}</span> {value}
+                <span>{key}</span> {strip(value)}
               </li>
             ))}
         </ul>
