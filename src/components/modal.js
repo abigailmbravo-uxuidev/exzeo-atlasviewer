8bce5d41e83e52d7fc8b543626bbae8d64132b28
Abby Bravo <abravo@exzeo.com>
2020-06-22 13:28:41 -0400

    error modal styling

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Modal = ({ message, closeModal, title = 'Error' }) => {
   return (
     <div className="modal fade-in">
-      <header>
-        <h4>{title}</h4>
-      </header>
-      <div className="body">{message}</div>
-      <footer>
-        <button className="actionBtn" type="button" onClick={closeModal}>
-          Close
-        </button>
-      </footer>
+      <div className="error-card">
+        <header>
+          <h4>{title}</h4>
+        </header>
+        <div className="modal-message">{message}</div>
+        <footer>
+          <button className="actionBtn" type="button" onClick={closeModal}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </footer>
+      </div>
     </div>
   );
 };
