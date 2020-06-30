e73375c1ac2490b70ac49cc27bc30aea4086c74d
jsutphin-1 <jsutphin@exzeo.com>
2020-06-30 14:08:23 -0400

    update overlay toggle

--- src/components/overlays.js
+++ src/components/overlays.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { faLayerGroup, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useUser } from '../context/user-context';
 import { useLayers, useSetLayers } from '../context/layer-context';
 
 const Overlays = ({ filter, setIsMapLoading }) => {
   const allLayers = useLayers();
   const setLayers = useSetLayers();
+  const [paneActive, setPaneActive] = useState(false);
+  const [paneHeight, setPaneHeightState] = useState();
+  const content = useRef(null);
+
+  const toggleAccordion = () => {
+    setPaneActive(paneActive ? false : true);
+    setPaneHeightState(
+      paneActive === true ? '0px' : `${content.current.scrollHeight}px`
+    );
+  };
 
   const toggleLayer = ({ target }) => {
     if (target.checked) setIsMapLoading(true);
@@ -19,6 +29,10 @@ const Overlays = ({ filter, setIsMapLoading }) => {
     });
     setLayers(newLayers);
   };
+
+  useEffect(() => {
+    setPaneHeightState(content.current.scrollHeight);
+  }, []);
 
   const layers =
     filter && filter.length > 1
@@ -32,26 +46,38 @@ const Overlays = ({ filter, setIsMapLoading }) => {
           <FontAwesomeIcon icon={faLayerGroup} />
           &nbsp;Overlay Layers
         </h4>
+        <button
+          className={`paneToggle ${!paneActive ? 'closed' : 'open'}`}
+          onClick={toggleAccordion}
+        >
+          <FontAwesomeIcon icon={faChevronDown} />
+        </button>
       </header>
-      <ul className="panel-list">
-        {layers &&
-          layers.map((layer, index) => (
-            <li key={layer._id}>
-              <span className="checkbox-wrapper wrapper">
-                <input
-                  type="checkbox"
-                  onChange={toggleLayer}
-                  value={layer._id}
-                />
-              </span>
-              <span className="feed-detail-wrapper wrapper">
-                <h5>
-                  <span className="overlay-name">{layer.name}</span>
-                </h5>
-              </span>
-            </li>
-          ))}
-      </ul>
+      <div
+        className={`pane ${!paneActive ? 'closed' : 'open'}`}
+        ref={content}
+        style={{ maxHeight: `${paneHeight}` }}
+      >
+        <ul className="panel-list">
+          {layers &&
+            layers.map((layer, index) => (
+              <li key={layer._id}>
+                <span className="checkbox-wrapper wrapper">
+                  <input
+                    type="checkbox"
+                    onChange={toggleLayer}
+                    value={layer._id}
+                  />
+                </span>
+                <span className="feed-detail-wrapper wrapper">
+                  <h5>
+                    <span className="overlay-name">{layer.name}</span>
+                  </h5>
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
