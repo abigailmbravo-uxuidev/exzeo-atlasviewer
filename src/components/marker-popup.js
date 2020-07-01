a633a9509e478569ee31d0bc69a11125af18f40d
Abby Bravo <abravo@exzeo.com>
2020-07-01 10:24:34 -0400

    new styling for popup on marker

--- src/components/marker-popup.js
+++ src/components/marker-popup.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const stripBreaks = value =>
   value && value.replace ? value.replace(/[<]br[^>]*[>]/gi, '') : value;
@@ -32,7 +34,25 @@ const MarkerPopup = ({ properties }) => {
 
   return (
     <div className="marker-popup">
-      <h2>{markerName.replace('<br/>', ' ')}</h2>
+      <div className="marker-popup-title-header">
+        {/* TO DO: @ERIC - this is the placeholder for the feed name */}
+        <p>[Feed Name]</p>
+      </div>
+      <div className="marker-popup-header">
+        <h2 className="title-tip" title={markerName.replace('<br/>', ' ')}>
+          {markerName.replace('<br/>', ' ')}
+        </h2>
+        <p>
+          {/* TO DO: @ERIC - here we just need help finding the status color so it properly displays the mapping color*/}
+          <span
+            className="icon-wrapper wrapper"
+            style={{ color: status.color }}
+          >
+            <FontAwesomeIcon icon={faCircle} />
+          </span>
+          {properties.PolicyStatus}
+        </p>
+      </div>
       <div className="popupContent">
         <ul>
           {properties &&
@@ -41,7 +61,6 @@ const MarkerPopup = ({ properties }) => {
             ))}
         </ul>
       </div>
-      <h4>{properties.PolicyStatus}</h4>
     </div>
   );
 };
