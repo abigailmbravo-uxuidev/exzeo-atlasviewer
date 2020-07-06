ba970fd93f4a02d576545cabf1ad081dbe822e59
elamison <elamison@exzeo>
2020-07-06 09:35:37 -0400

    Hook up tool tips..

--- src/components/view.js
+++ src/components/view.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { format } from 'date-fns';
 import ReactTooltip from 'react-tooltip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -98,86 +99,118 @@ const View = ({ setBasemap }) => {
           <ul className="panel-list">
             {inViewFeeds.length > 0 &&
               inViewFeeds.map((feed, index) => (
-                <Fragment key={feed._id}>
-                  <li key={feed._id}>
-                    {popouts.includes(feed._id) && (
-                      <ViewPopout feed={feed} close={togglePopout} />
+                <li key={feed._id}>
+                  {popouts.includes(feed._id) && (
+                    <ViewPopout feed={feed} close={togglePopout} />
+                  )}
+                  <span
+                    className="eyeball-wrapper wrapper"
+                    role="button"
+                    tabIndex={index}
+                    onClick={() => toggleFeed(feed)}
+                    onKeyDown={() => toggleFeed(feed)}
+                  >
+                    {feed.active ? (
+                      <FontAwesomeIcon icon={faEye} />
+                    ) : (
+                      <FontAwesomeIcon icon={faEyeSlash} />
                     )}
-                    <span
-                      className="eyeball-wrapper wrapper"
-                      role="button"
-                      tabIndex={index}
-                      onClick={() => toggleFeed(feed)}
-                      onKeyDown={() => toggleFeed(feed)}
-                    >
-                      {feed.active ? (
-                        <FontAwesomeIcon icon={faEye} />
-                      ) : (
-                        <FontAwesomeIcon icon={faEyeSlash} />
-                      )}
-                    </span>
-                    <span className="feed-detail-wrapper wrapper">
+                  </span>
+                  <span className="feed-detail-wrapper wrapper">
+                    <>
                       <h5>
                         <span
                           className="overlay-name"
-                          data-tip
-                          data-for="feedTooltip"
+                          data-tip={feed.name}
+                          data-for={`feedTooltip-${feed._id}`}
                         >
                           {feed.name}
                         </span>
                       </h5>
-                    </span>
-                    <span className="icon-popOut">
-                      <FontAwesomeIcon
-                        icon={faExternalLinkSquareAlt}
-                        onClick={() => togglePopout(feed._id)}
-                      />
-                    </span>
-                    {/* if details/statuses exist show loop and total --------------------------------------- */}
-                    <ul className="detail-list">
-                      {feed.statuses &&
-                        feed.statuses.length > 0 &&
-                        feed.statuses.map((status, index) => (
-                          <li key={index}>
-                            <span
-                              className="eyeball-wrapper wrapper"
-                              role="button"
-                              tabIndex={index}
-                              onClick={() => toggleStatus(feed, status.name)}
-                              onKeyDown={() => toggleStatus(feed, status.name)}
-                            >
-                              {!feed.filter ||
-                              !feed.filter.includes(status.name) ? (
-                                <FontAwesomeIcon icon={faEye} />
-                              ) : (
-                                <FontAwesomeIcon icon={faEyeSlash} />
-                              )}
-                            </span>
-                            <span
-                              className="icon-wrapper wrapper"
-                              style={{ color: status.color }}
-                            >
-                              <FontAwesomeIcon icon={faCircle} />
-                              {/* maps to icon and color from feed */}
-                            </span>
-                            <span className="feed-detail-wrapper wrapper">
-                              <h6>
-                                <span className="detail-name">
-                                  {status.name}
-                                </span>
-                              </h6>
-                            </span>
-                            <span className="detail-count">{status.count}</span>
-                          </li>
-                        ))}
-                    </ul>
-                    <div className="total-count">
-                      <span>Total:&nbsp;</span>
-                      {feed.total}
-                    </div>
-                    {/* end of feed detail loop */}
-                  </li>
-                </Fragment>
+                      <ReactTooltip
+                        className="panel-tooltip"
+                        id={`feedTooltip-${feed._id}`}
+                        place="right"
+                        effect="float"
+                        type="light"
+                        delayHide={500}
+                        delayShow={200}
+                        backgroundColor="#fff"
+                        border={true}
+                        borderColor="#ddd"
+                      >
+                        <h5>{feed.name}</h5>
+                        <dl>
+                          <span>
+                            <dt>Created</dt>
+                            <dd>
+                              {feed.created_at &&
+                                format(new Date(feed.created_at), 'MM-dd-yyyy')}
+                            </dd>
+                          </span>
+                          <span>
+                            <dt>Updated</dt>
+                            <dd>
+                              {feed.updated_at &&
+                                format(new Date(feed.updated_at), 'MM-dd-yyyy')}
+                            </dd>
+                          </span>
+                          <span>
+                            <dt>Author</dt>
+                            <dd>{feed.owner.name}</dd>
+                          </span>
+                        </dl>
+                      </ReactTooltip>
+                    </>
+                  </span>
+                  <span className="icon-popOut">
+                    <FontAwesomeIcon
+                      icon={faExternalLinkSquareAlt}
+                      onClick={() => togglePopout(feed._id)}
+                    />
+                  </span>
+                  {/* if details/statuses exist show loop and total --------------------------------------- */}
+                  <ul className="detail-list">
+                    {feed.statuses &&
+                      feed.statuses.length > 0 &&
+                      feed.statuses.map((status, index) => (
+                        <li key={index}>
+                          <span
+                            className="eyeball-wrapper wrapper"
+                            role="button"
+                            tabIndex={index}
+                            onClick={() => toggleStatus(feed, status.name)}
+                            onKeyDown={() => toggleStatus(feed, status.name)}
+                          >
+                            {!feed.filter ||
+                            !feed.filter.includes(status.name) ? (
+                              <FontAwesomeIcon icon={faEye} />
+                            ) : (
+                              <FontAwesomeIcon icon={faEyeSlash} />
+                            )}
+                          </span>
+                          <span
+                            className="icon-wrapper wrapper"
+                            style={{ color: status.color }}
+                          >
+                            <FontAwesomeIcon icon={faCircle} />
+                            {/* maps to icon and color from feed */}
+                          </span>
+                          <span className="feed-detail-wrapper wrapper">
+                            <h6>
+                              <span className="detail-name">{status.name}</span>
+                            </h6>
+                          </span>
+                          <span className="detail-count">{status.count}</span>
+                        </li>
+                      ))}
+                  </ul>
+                  <div className="total-count">
+                    <span>Total:&nbsp;</span>
+                    {feed.total}
+                  </div>
+                  {/* end of feed detail loop */}
+                </li>
               ))}
           </ul>
           {/* end of active feeds loop */}
@@ -202,15 +235,31 @@ const View = ({ setBasemap }) => {
                     )}
                   </span>
                   <span className="feed-detail-wrapper wrapper">
-                    <h5>
-                      <span
-                        className="overlay-name"
-                        data-tip
-                        data-for="layerTooltip"
+                    <>
+                      <h5>
+                        <span
+                          className="overlay-name"
+                          data-tip={layer.name}
+                          data-for={`feedTooltip-${layer._id}`}
+                        >
+                          {layer.name}
+                        </span>
+                      </h5>
+                      <ReactTooltip
+                        className="tooltip"
+                        id={`feedTooltip-${layer._id}`}
+                        place="right"
+                        effect="float"
+                        type="light"
+                        delayHide={500}
+                        delayShow={200}
+                        backgroundColor="#fff"
+                        border={true}
+                        borderColor="#ddd"
                       >
-                        {layer.name}
-                      </span>
-                    </h5>
+                        <h5>{layer.name}</h5>
+                      </ReactTooltip>
+                    </>
                   </span>
                 </li>
               ))}
@@ -235,52 +284,6 @@ const View = ({ setBasemap }) => {
           <Icon />
         </button>
       </div>
-      {/* TO DO: @ERIC - the tooltips aren't working.
-        I think it is due to these being linked to the ID and ID isn't currently dynamic.
-        I am unsure how to connect, but if you can get this one, I should be able to figure out the rest of them.
-      */}
-      <ReactTooltip
-        className="panel-tooltip"
-        id="feedTooltip"
-        place="right"
-        effect="float"
-        type="light"
-        delayHide={500}
-        delayShow={200}
-        backgroundColor="#fff"
-        border={true}
-        borderColor="#ddd"
-      >
-        <h5>[ Feed Name ]</h5>
-        <dl>
-          <span>
-            <dt>Created</dt>
-            <dd>[ Created ]</dd>
-          </span>
-          <span>
-            <dt>Updated</dt>
-            <dd>[ Updated ]</dd>
-          </span>
-          <span>
-            <dt>Author</dt>
-            <dd>[ Author ]</dd>
-          </span>
-        </dl>
-      </ReactTooltip>
-      <ReactTooltip
-        className="tooltip"
-        id="layerTooltip"
-        place="right"
-        effect="float"
-        type="light"
-        delayHide={500}
-        delayShow={200}
-        backgroundColor="#fff"
-        border={true}
-        borderColor="#ddd"
-      >
-        <h5>[ Layer Name ]</h5>
-      </ReactTooltip>
     </div>
   );
 };
