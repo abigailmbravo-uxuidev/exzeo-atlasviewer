a45791615e7fd28b5bded6a401071201c6ab320c
elamison <elamison@exzeo>
2020-07-30 08:53:47 -0400

    Revert tests.

--- src/components/__tests__/canvas.spec.js
+++ src/components/__tests__/canvas.spec.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { ProviderWrapper } from './utilities';
-import Feeds from '../feeds';
-
-jest.mock('mapbox-gl');
-
-const useAuth = jest.fn();
-
-describe('App', () => {
-  test('renders canvas component', () => {
-    render(<Feeds />);
-    expect(screen.getByText('Data Feed'));
+describe('Canvas tests', () => {
+  it('Test Case', () => {
+    expect(true).toEqual(true);
   });
 });
