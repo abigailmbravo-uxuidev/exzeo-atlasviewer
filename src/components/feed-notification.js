4bb11340ab9c625909440948de6712602d4ac999
elamison <elamison@exzeo>
2020-08-12 08:35:00 -0400

    Update notification card.

--- src/components/feed-notification.js
+++ src/components/feed-notification.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileExport } from '@fortawesome/free-solid-svg-icons';
+import { faFileExport, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const FeedNotification = ({ feed, close }) => {
   if (!feed) return null;
   return (
-    <div className="new-feed-notification" key={feed._id}>
-      <FontAwesomeIcon icon={faFileExport} />
-      New Shared Feed
+    <div className="new-feed-notification card fade-in" key={feed._id}>
+      <header>
+        <button className="actionBtn" type="button" onClick={close}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <FontAwesomeIcon icon={faFileExport} /> New Shared Feed
+      </header>
+      <div className="feed-name">{feed.name}</div>
     </div>
   )
 }
