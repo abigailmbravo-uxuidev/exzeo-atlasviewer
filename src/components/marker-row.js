1faa0a91902571994421b34948be5e7e929f47cf
elamison <elamison@exzeo>
2020-08-18 11:18:37 -0400

    Filter lat/lon from marker-row.

--- src/components/marker-row.js
+++ src/components/marker-row.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 import { convertBreaks } from '../utils/utils';
+
+const doNotShow = ['lat', 'lon', 'lng', 'latitude', 'longitude'];
 
 const MarkerRow = ({ column, value }) => {
   const filter = ['name', 'status_name', 'status_color', 'symbol'];
+  const columnName = column.toLowerCase();
 
-  if (column.toLowerCase().includes('-url')) {
+  if (doNotShow.includes(columnName)) return null;
+
+  let formattedValue = value;
+
+  if (columnName.includes('-url')) {
     return (
       <li key={column}>
         <span>
@@ -17,10 +25,14 @@ const MarkerRow = ({ column, value }) => {
     );
   }
 
-  return filter.includes(column.toLowerCase()) ? null : (
+  if (columnName.includes('-date')) {
+    formattedValue = format(new Date(formattedValue), 'MM-dd-yyyy');
+  }
+
+  return filter.includes(columnName) ? null : (
     <li key={column}>
       <span>{column.replace(/-dollar|-date/i, '')}</span>
-      {convertBreaks(value)}
+      {convertBreaks(formattedValue)}
     </li>
   );
 };
