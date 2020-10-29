59dc24639d25a908844d276c4713301332295840
jsutphin-1 <jsutphin@exzeo.com>
2020-10-29 08:26:18 -0400

    rebase

--- src/components/view.js
+++ src/components/view.js
@@ -28,11 +28,6 @@ const View = ({ setBasemap, viewActive, setViewState }) => {
     setViewState(false);
   }, [setViewState]);
 
-  useEffect(() => {
-    const feedsInView = feeds.some(feed => feed.inView);
-    setViewState(feedsInView);
-  }, [feeds, viewActive]);
-
   const inViewFeeds = feeds.filter(feed => feed.inView);
   inViewFeeds.forEach(
     feed =>
